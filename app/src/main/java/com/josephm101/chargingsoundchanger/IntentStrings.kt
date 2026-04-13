bda9738aa1c0612053fca6672a4c1a95afe2e46c
Joseph Mignone <jmig3066@gmail.com>
2026-04-13 13:46:05 -0400

    Delete IntentStrings.kt (not needed anymore)

--- app/src/main/java/com/josephm101/chargingsoundchanger/IntentStrings.kt
+++
@@ -1,7 +0,0 @@
-package com.josephm101.chargingsoundchanger
-
-class IntentStrings {
-    companion object {
-        //val ChargingSoundServiceTestSoundIntent = "com.josephm101.chargingsoundservice.testsound"
-    }
-}
